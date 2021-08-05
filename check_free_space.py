64121526a4dea5f35840ba9619e632219bbe69bc
zhiqiuli <zhiqiuli.edu@gmail.com>
2021-08-05 17:00:53 -0400

    Add files via upload

---
+++ check_free_space.py
@@ -0,0 +1,5 @@
+for i in range(10):
+    print(i)
+
+print('Everything is OK.')
+print('OK')
---
+++ free_memory.py
@@ -0,0 +1,5 @@
+def main():
+    ''' check if there is enough free memory in the computer '''
+    print('Everything is OK')
+
+main()
