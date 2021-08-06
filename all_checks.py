fd55abd7d5257139f5971e8d756589969fca1186
Zhiqiu Li <zhiqiuli.edu@gmail.com>
2021-08-06 09:42:06 -0400

    add a simple network connection check

--- all_checks.py
+++ all_checks.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import os
 import sys
+import socket
+import psutil
 
 def check_reboot():
     return os.path.exists("/run/reboot-required")
@@ -12,9 +14,18 @@ def check_root_full():
 def check_if_everything_true():
     return True
 
+def check_no_network():
+    '''return true if it fails to resolve google's url'''
+    try:
+        socket.gethostname('www.google.com')
+        return False
+    Except:
+        return True
+
 def main():
     checks = [(check_reboot(), 'pending reboot'),
-              (check_if_everything_true(), 'nothing is wrong')]
+              (check_root_full(), 'root partition full'),]
+              (check_no_network(), 'no internet connection')]
     everything_ok = True
     for check, msg in checks:
         if check():
