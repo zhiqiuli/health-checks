9e707a04221fbb6dc417d204ed72ca66d9dde879
Zhiqiu Li <zhiqiuli.edu@gmail.com>
2021-08-06 09:07:04 -0400

    create a wrapper function for

--- all_checks.py
+++ all_checks.py
@@ -5,10 +5,16 @@ import sys
 def check_reboot():
     return os.path.exists("/run/reboot-required")
 
+def check_root_full():
+    ''' Return True'''
+    return check_disk_full()
+
 def main():
     if check_reboot():
         print("Pending Reboot.")
         sys.exit(1)
+    if check_root_full():
+        print("check if root space is available")    
     print("Another statement says everything here is OK.")
     print("Everything ok.")
     print("Yes - everything is ok.")
